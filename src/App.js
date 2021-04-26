93fae087ac5586179fb3a04532ef00c7aec8b9d9
khangtran <tqkhang93@gmail.com>
2021-04-26 15:42:12 +0700

    4 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React from "react";
 import "./style.css";
+import GameCore from "./GameCore";
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+export default class App extends React.Component {
+  componentDidMount() {
+    GameCore.test();
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="ui-game">
+          <canvas id="canvas" />
+
+          <div className="ui score">
+            <span id="lb-level">Level 1: 10</span>
+            <br />
+            <span id="lb-score">Điểm 0</span>
+            <br />
+            <span id="lb-mutil" />
+          </div>
+
+          <div className="ui detect">
+            <span id="lb-detect">Nhận dạng ...</span>
+          </div>
+        </div>
+
+        <div id="ui-lobby" className="ui lobby">
+          <button id="bt-play" className="bt-play">
+            Tìm trận
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
